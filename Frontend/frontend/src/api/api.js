b7f9506ffe153c1ea19ff1a19e20abadd699eefc
nobitamk <muralikotha37@gmail.com>
2026-03-08 22:29:12 +0530

    updated api url to deployed backend

--- Frontend/frontend/src/api/api.js
+++ Frontend/frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://leave-management-system-9rbd.onrender.com/api"
 });
 
 export default API;
